0a5ce4458277165f893856b0372ace20662ad086
tianzhi yang <tianzhiyang@tianzhideMacBook-Air.local>
2024-10-20 22:40:15 +0800

    init

---
+++ cs193p/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cs193p
+//
+//  Created by tianzhi yang on 2024/10/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
